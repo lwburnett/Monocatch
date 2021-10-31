684ee599f9c8bb269f14ef52b0880f86a5dd335f
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2021-10-31 10:07:53 -0600

    Renaming some files and classes

--- Monocatch/Monocatch/GameMaster.cs
+++ Monocatch/Monocatch/GameMaster.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Monocatch
 {
-    public class Game1 : Game
+    public class GameMaster : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public GameMaster()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- Monocatch/Monocatch/Program.cs
+++ Monocatch/Monocatch/Program.cs
@@ -5,10 +5,10 @@ namespace Monocatch
     public static class Program
     {
         [STAThread]
-        static void Main()
+        private static void Main()
         {
-            using (var game = new Game1())
-                game.Run();
+            using var game = new GameMaster();
+            game.Run();
         }
     }
 }
